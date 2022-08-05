a19518a9c47b547bc87763e9883ab31df58c6cf5
009 Nattawut Piyachan <ji560chan@gmail.com>
2022-08-05 13:17:43 +0700

    first commit

---
+++ form/-index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>index</title>
+  </head>
+  <body>
+
+<?php
+include("-header.php");
+?>
+
+<div id="carouselExampleControls" class="carousel dark slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://cdn.discordapp.com/attachments/647449037871513610/1004954867095318568/clay-banks-hwLAI5lRhdM-unsplash.jpg" class="d-block w-100" alt="..." width="600" height="700">
+    </div>
+    <div class="carousel-item">
+      <img src="https://cdn.discordapp.com/attachments/647449037871513610/1004955079373242368/yu-kato-824OwkP7sgk-unsplash.jpg" class="d-block w-100" alt="..."width="600" height="700">
+    </div>
+    <div class="carousel-item">
+    <img src="https://cdn.discordapp.com/attachments/647449037871513610/1004954866684280882/david-edelstein-N4DbvTUDikw-unsplash.jpg" class="d-block w-100" alt="..." width="600" height="700">
+    </div></a>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+
+<?php
+include("-footer.php");
+?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+</body>
+</html>
